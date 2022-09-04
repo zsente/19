baa5edda77cafa444a4227e460363b36bb8cb525
zsente <93928387+zsente@users.noreply.github.com>
2022-09-04 15:54:12 +0200

    add new functions

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,6 +1,6 @@
-int ft_isasnum(int c)
+int ft_isalnum(int c)
 {
-    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <=tou 'Z') || (c >= 0 && c <= 9))
+    if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 0 && c <= 9))
     {
         return(1);
     }
---
+++ ft_toupper.c
@@ -0,0 +1,11 @@
+int ft_toupper(int c)
+{
+    if(c >= 'a' && c <= 'z')
+    {
+        return(c - 32); 
+    }
+    else 
+    {
+        return(c);
+    }
+}
